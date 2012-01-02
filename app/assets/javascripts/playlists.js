1b577e32bb750aff31fb1383afaf77583690bf72
Federico Builes <federico.builes@gmail.com>
2012-01-01 21:01:14 -0500

    Clear the playlists div on recreation.

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,14 +1,29 @@
 $(function(){
-  var loading = function() {
+
+  var write = function(data) {
+    $("div#playlist").text("");
+    $("div#playlist").append(data);
+  }
+  var showLoading = function() {
     $("div.overlay, div#spinner").fadeIn();
   };
 
-  var success = function(evt, data, status, xhr) {
+  var hideLoading = function() {
     $("div.overlay, div#spinner").fadeOut();
-    $("div#playlist").append(data);
+  };
+
+  var error = function() {
+    hideLoading();
+    write("There has been an error processing the data. Please try again.");
+  };
+
+  var success = function(evt, data, status, xhr) {
+    hideLoading();
+    write(data);
   };
 
  $("#new_playlist")
-    .live("ajax:beforeSend",  loading)
+    .live("ajax:beforeSend", showLoading)
+    .live("ajax:error",  error)
     .live("ajax:success", success);
 });
