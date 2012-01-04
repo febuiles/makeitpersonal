7fcea2862b155afbfd98ee02d50f5259b4640618
Federico Builes <federico.builes@gmail.com>
2012-01-04 16:05:22 -0500

    Fixing SongPersistable.

--- app/concerns/song_persistable.rb
+++ app/concerns/song_persistable.rb
@@ -5,7 +5,7 @@ module SongPersistable
   end
 
   def fetch_songs
-    if songs.empty?
+    if songs.blank?
       begin
         self.songs = LastFm::List.new(username).between(start_date, end_date).to_json
       rescue OpenURI::HTTPError
--- spec/concerns/song_persistable_spec.rb
+++ spec/concerns/song_persistable_spec.rb
@@ -3,6 +3,15 @@ require 'fast_spec_helper'
 require 'song_persistable'
 require 'last_fm/list'
 
+module LastFm
+  class List
+    def initialize(user)
+      @user = user.strip
+      mock_document
+    end
+  end
+end
+
 describe SongPersistable do
   include SongPersistable
 
@@ -14,12 +23,13 @@ describe SongPersistable do
     let(:songs) { [] }
 
     it "doesn't reassign songs" do
-      self.stub!(:songs).and_return(song_list)
+      songs.stub!(:blank?).and_return(false)
       self.should_not_receive(:songs=)
       fetch_songs
     end
 
     it "saves the songs as an array of JSON objects" do
+      songs.stub!(:blank?).and_return(true)
       expected_songs = ["{\"artist\":\"Foals\",\"title\":\"Heavy Water\",\"art\":\"http://userserve-ak.last.fm/serve/34s/70521468.png\",\"time\":\"2011-12-22 13:46:49 -0500\"}", "{\"artist\":\"Alexisonfire\",\"title\":\"The Kennedy Curse\",\"art\":\"http://userserve-ak.last.fm/serve/34s/12774691.jpg\",\"time\":\"2011-12-22 13:43:02 -0500\"}"]
       self.should_receive(:songs=).with(expected_songs)
       fetch_songs
