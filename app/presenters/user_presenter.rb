51d7dad094125e7d1806a0656014435d8717ca78
Federico Builes <federico.builes@gmail.com>
2012-11-04 23:50:41 -0500

    Show the followers in a nicer way.

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module UserPresenter
   def display_name
     if name.present?
@@ -5,5 +7,10 @@ module UserPresenter
     else
       username
     end
+  end
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "<img class='avatar' src='http://www.gravatar.com/avatar/#{hash}?d=mm' />".html_safe
   end
 end
