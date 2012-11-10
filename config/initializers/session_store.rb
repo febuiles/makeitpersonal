cf1d7011ea08efb015e9c81f1f5cb36a74edabc8
Federico Builes <federico.builes@gmail.com>
2012-11-10 01:50:05 -0500

    Save the sessions in the database (CookieStore won't cut it anymore).

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Makeitpersonal::Application.config.session_store :cookie_store, key: '_last_playlist_session'
+Makeitpersonal::Application.config.session_store :active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121102051336) do
+ActiveRecord::Schema.define(:version => 20121110064403) do
 
   create_table "api_requests", :force => true do |t|
     t.string   "endpoint"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(:version => 20121102051336) do
   add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
   add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
   add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "songs", :force => true do |t|
     t.text     "lyrics"
