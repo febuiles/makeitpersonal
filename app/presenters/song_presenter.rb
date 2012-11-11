46bcdc9a67a34a52cc08a910c7118a0c338eeea1
Federico Builes <federico.builes@gmail.com>
2012-11-11 18:09:02 -0500

    Show all the sidenotes.

--- app/presenters/song_presenter.rb
+++ app/presenters/song_presenter.rb
@@ -24,15 +24,11 @@ module SongPresenter
     end
   end
 
-  # TODO: I got 99 problems... and these 3 regexes are in that list.
   def body
     body = lyrics.gsub(/\*(.*?)\*/m, '<em>\1</em>') # emph
-    i = 1
-    while body.match(SIDENOTE_REGEX) # colorize the sidenotes
-      body.sub!(SIDENOTE_REGEX, "<span class='sidenote'>[#{i}]</span>")
-      i += 1
+    body.scan(SIDENOTE_REGEX).count.times do |i|
+      body.sub!(SIDENOTE_REGEX, "<span class='sidenote'>[#{i + 1}]</span>")
     end
-    body.gsub!(/\n<span class='sidenote'>\[\d+\]<\/span>(\r\n|$)/m, "") # remove the notes on empty lines
     body.replace_newlines
   end
 end
