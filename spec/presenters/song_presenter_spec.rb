39d883844b8629daa7013097ec09c3c77a50e0cc
Federico Builes <federico.builes@gmail.com>
2012-11-11 18:09:47 -0500

    Fixing SongPresenter specs.

--- spec/presenters/song_presenter_spec.rb
+++ spec/presenters/song_presenter_spec.rb
@@ -2,15 +2,10 @@ require 'spec_helper'
 
 describe SongPresenter do
   describe "body" do
-  it "replaces the sidenotes with bracketed numbers" do
-    song = Song.new(lyrics: "I swear he's [[made of bone]]")
-    song.body.should == "I swear he's <span class='sidenote'>[1]</span>"
-  end
-
-  it "doesn't leave trailing sidenotes" do
-    song = Song.new(lyrics: "I swear he's [[made of bone]]\n[[test]]")
-    song.body.should == "I swear he's <span class='sidenote'>[1]</span>"
-  end
+    it "replaces the sidenotes with bracketed numbers" do
+      song = Song.new(lyrics: "I swear he's [[made of bone]]")
+      song.body.should == "I swear he's <span class='sidenote'>[1]</span>"
+    end
   end
 
   describe "embed" do
