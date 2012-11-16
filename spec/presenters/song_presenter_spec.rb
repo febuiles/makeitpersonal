abb0d4e8517d8c2e73ac7661b86b2045ed9cfe0c
Federico Builes <federico.builes@gmail.com>
2012-11-15 21:35:45 -0500

    Fixing SongPresenter specs.

--- spec/presenters/song_presenter_spec.rb
+++ spec/presenters/song_presenter_spec.rb
@@ -4,7 +4,7 @@ describe SongPresenter do
   describe "body" do
     it "replaces the sidenotes with bracketed numbers" do
       song = Song.new(lyrics: "I swear he's [[made of bone]]")
-      song.body.should == "I swear he's <span class='sidenote'>[1]</span>"
+      song.body.should == "I swear he's <span class='sidenote-ref'>[1]</span>"
     end
   end
 
