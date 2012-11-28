ec2297d62cc66f7d3c55cc29d823fd411dd65655
Federico Builes <federico.builes@gmail.com>
2012-11-27 21:09:02 -0500

    Show a welcome notice to first time users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :show_welcome_notice
+
   protect_from_forgery
   def after_sign_in_path_for(resource)
     account_path
@@ -6,6 +8,13 @@ class ApplicationController < ActionController::Base
 
   def render_not_found
     raise ActiveRecord::RecordNotFound.new('Not Found')
+  end
+
+  def show_welcome_notice
+    if request.format.html? && !current_user && session[:first_time].nil?
+      @show_welcome_notice = true
+      session[:first_time] = false
+    end
   end
 
   protected
