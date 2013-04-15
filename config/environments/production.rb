8e00cbe9ed987c35ab3136f795abc0b0c68cedb0
Federico Builes <federico.builes@gmail.com>
2013-04-15 18:30:20 -0300

    Enable StarTLS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Makeitpersonal::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'makeitpersonal.co'
+    :domain         => 'makeitpersonal.co',
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'makeitpersonal.co' }
