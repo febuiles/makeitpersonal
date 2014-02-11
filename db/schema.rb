01bf5be0b828a92d981cc23311f1486e57ae9739
Federico Builes <federico.builes@gmail.com>
2014-02-11 18:49:12 -0500

    Updating schema.

--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(:version => 20140129034552) do
     t.text     "lyrics"
     t.string   "artist"
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.string   "youtube_url"
     t.integer  "user_id"
     t.string   "slug"
