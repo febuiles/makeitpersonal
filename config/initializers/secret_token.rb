8422f91a73a39755073efffda0d33ce028b44212
Federico Builes <federico.builes@gmail.com>
2014-06-30 19:25:45 -0500

    Update sample token file.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Makeitpersonal::Application.config.secret_token = ENV['secret'] || '26e3af10ac38dfb80d5a9262c634896e'
+Makeitpersonal::Application.config.secret_token = ENV['secret'] || 'beb7bad007016aed01067e4cfae2a0fa5ea31a538fe3904d528a69a17bc0eccff05f16d35516f3cd815f274f9bd74743806aa92e700a4c740b2c01a6e6746b2f'
