2e78298bd73b5138a87d4543ddf7514d86f0322d
Federico Builes <f.builes@catawiki.nl>
2017-09-29 12:39:17 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,37 +1,12 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "rails/test_unit/railtie"
-require "sprockets/railtie"
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(*Rails.groups)
 
 module Makeitpersonal
   class Application < Rails::Application
-    config.time_zone = 'Eastern Time (US & Canada)'
-    config.encoding = "utf-8"
-    config.assets.enabled = true
-    config.autoload_paths += %W(#{config.root}/app/presenters)
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/lib/lyrics)
-    config.assets.version = '2.0'
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
-    config.assets.paths << "#{Rails.root}/vendor/assets/javascripts"
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
-
-    config.generators do |g|
-      g.orm :active_record
-    end
-
-    config.assets.initialize_on_precompile = false
-    config.assets.precompile += ["song/song.css", "landing.css", "pages.css"]
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
   end
 end
