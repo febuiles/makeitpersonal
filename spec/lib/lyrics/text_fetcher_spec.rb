9ccfca546554b91e9c957bbc7fa35cc0449fad71
Federico Builes <f.builes@catawiki.nl>
2018-10-03 13:45:27 +0200

    Updating specs in lib/lyrics.

--- spec/lib/lyrics/text_fetcher_spec.rb
+++ spec/lib/lyrics/text_fetcher_spec.rb
@@ -8,31 +8,31 @@ describe TextFetcher do
 
   context "artist and song names" do
     it "formats the artist and the title" do
-      fetcher.artist.should == "The_Gaslight_Anthem"
-      fetcher.title.should == "Great_Expectations"
+      expect(fetcher.artist).to eq("The_Gaslight_Anthem")
+      expect(fetcher.title).to eq("Great_Expectations")
     end
 
     it "cleans the artist and song title" do
       fetcher = TextFetcher.new(" The Gaslight anthem ", " great expectations ")
-      fetcher.artist.should == "The_Gaslight_Anthem"
-      fetcher.title.should == "Great_Expectations"
+      expect(fetcher.artist).to eq("The_Gaslight_Anthem")
+      expect(fetcher.title).to eq("Great_Expectations")
     end
   end
 
   context "#lyrics_url" do
     it "encodes the url" do
       fetcher = TextFetcher.new("Kraftwerk", "Geigerzähler")
-      fetcher.lyrics_url.should == "http://lyrics.wikia.com/index.php?title=Kraftwerk:Geigerz%C3%A4hler&action=edit"
+      expect(fetcher.lyrics_url).to eq("http://lyrics.wikia.com/index.php?title=Kraftwerk:Geigerz%C3%A4hler&action=edit")
     end
 
     it "accepts ampersands in the artist's name" do
       fetcher = TextFetcher.new("Coheed & Cambria", "Neverender")
-      fetcher.lyrics_url.should == "http://lyrics.wikia.com/index.php?title=Coheed_%26_Cambria:Neverender&action=edit"
+      expect(fetcher.lyrics_url).to eq("http://lyrics.wikia.com/index.php?title=Coheed_%26_Cambria:Neverender&action=edit")
     end
 
     it "accepts ampersands in the song's name" do
       fetcher = TextFetcher.new("Coheed & Cambria", "Always & Never")
-      fetcher.lyrics_url.should == "http://lyrics.wikia.com/index.php?title=Coheed_%26_Cambria:Always_%26_Never&action=edit"
+      expect(fetcher.lyrics_url).to eq("http://lyrics.wikia.com/index.php?title=Coheed_%26_Cambria:Always_%26_Never&action=edit")
     end
   end
 end
